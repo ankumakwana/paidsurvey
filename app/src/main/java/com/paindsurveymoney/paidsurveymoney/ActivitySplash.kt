34df7a1083a24490ea0e40f96019a5e4fe9f2483
ankitamakwana <ankita@webrexstudio.com>
2018-06-27 11:52:38 +0530

    intial commit

---
+++ app/src/main/java/com/paindsurveymoney/paidsurveymoney/ActivitySplash.kt
@@ -0,0 +1,15 @@
+package com.paindsurveymoney.paidsurveymoney
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Webrex Studio on 6/27/2018.
+ */
+class ActivitySplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
